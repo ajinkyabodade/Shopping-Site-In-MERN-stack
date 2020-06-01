6ba69e13209521ca50626e0c0f631669654cbc7c
Ajinkya <bodadeajinkya@gmail.com>
2020-06-01 22:40:38 +0530

    product controller v3

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -139,3 +139,55 @@ exports.updateProduct = (req,res) =>{
         })
     })
 }
+
+exports.getAllProducts=(req,res)=>{
+    let limit =req.query.limit ? parseInt(req.query.limit) :8;
+    let sortBy =req.query.sortBy ? parseInt(req.query.sortBy) :"_id";
+
+    Product.find()
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy,"asc"]])
+    .limit(limit)
+    .exec((err,products)=>{
+        if(err){
+            return res.status(400).json({
+                error:"No Products found"
+            })
+        }
+        res.json(products)
+    })
+}
+
+
+exports.updateStock =(req,res,next)=>{
+    let myOperations = req.body.order.products.map(prod=>{
+        return{
+            updateOne:{
+                filter:{_id:prod._id},
+                update:{$inc:{stock:-prod.count,sold:+prod.count}}
+            }
+        }
+    })
+
+    Product.bulkWrite(myOperations,{},(err,products)=>{
+        if(err){
+            return res.status(400).json({ 
+                error:"Bulk operation failed"
+             })
+        }
+    })
+
+}
+
+
+exports.getAllUniqueCategories =(req,res)=>{
+    Product.distinct("category",{},(err,category)=>{
+        if(err){
+            res.status(400).json({
+                error:"No category found"
+            })
+        }
+        res.json(category);
+    })
+}
